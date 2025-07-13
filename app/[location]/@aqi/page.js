02b2cc21a4313d07bbc040d3bcd075f563b77b54
Mohammad Al Samiul <alsamiul123@gmail.com>
2025-07-13 14:47:44 +0600

    pararrel routes added

---
+++ app/[location]/@aqi/page.js
@@ -0,0 +1,3 @@
+export default function AQI() {
+  return <div>AQI</div>;
+}
---
+++ app/[location]/layout.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+export default function CommonLayout({
+  children,
+  aqi,
+  temperature,
+  weather,
+  wind,
+}) {
+  return (
+    <>
+      <div className="wrapper">
+        <div className="overlay"></div>
+        <Image
+          src="/images/background.png"
+          className="bg-img"
+          alt="backgorund-image"
+          width={700}
+          height={1200}
+        />
+
+        <main className="!z-50 w-full">
+          <div className="container">
+            <div className="grid grid-cols-12 gap-y-8 py-16 lg:gap-8 2xl:gap-20 2xl:py-20">
+              {children}
+              {aqi}
+              {temperature}
+              {weather}
+              {wind}
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
