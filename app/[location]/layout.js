151b44192c7589f5443a6ac9077acc5209606bc3
Mohammad Al Samiul <alsamiul123@gmail.com>
2025-07-16 11:33:08 +0600

    temperature wind weather components added

--- app/[location]/layout.js
+++ app/[location]/layout.js
@@ -10,23 +10,22 @@ export default function CommonLayout({
   return (
     <>
       <div className="wrapper">
+        <div className="overlay"></div>
         <Image
           src="/images/background.png"
           className="bg-img"
-          alt="backgorund-image"
           width={700}
           height={1200}
+          alt="background-image"
         />
-        <div className="overlay"></div>
-
         <main className="!z-50 w-full">
           <div className="container">
             <div className="grid grid-cols-12 gap-y-8 py-16 lg:gap-8 2xl:gap-20 2xl:py-20">
               {children}
-              {aqi}
-              {temperature}
               {weather}
+              {aqi}
               {wind}
+              {temperature}
             </div>
           </div>
         </main>
